da410699615e2f694351b07291ac94ac58c83b00
jaymuk <jaymuk.business@gmail.com>
2015-03-31 17:37:00 +0100

    Can select ship

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -24,6 +24,14 @@ class Battleships < Sinatra::Base
     erb :game
   end
 
+  get '/hit' do
+    @visitor = params[:player]
+    @player = Player.new @visitor
+    @board = Board.new({size: 25, cell: Cell, number_of_pieces: 3})
+    @ship = Ship.new({size: 3})
+    erb :hit
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
