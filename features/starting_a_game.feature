6f69864bd8ba0a0cca023b23d42736cffd2de7ea
jaymuk <jaymuk.business@gmail.com>
2015-03-31 10:46:08 +0100

    Change battleships web app feature tests

---
+++ features/starting_a_game.feature
@@ -0,0 +1,11 @@
+Feature: Starting the game
+  In order to play battleships
+  As a nostalgic player
+  I want to start a new game
+
+  Scenario: Registering
+    Given I am on the homepage
+    Then I should see "What's your name?"
+    When I press "Start Game"
+
+
---
+++ lib/battleships.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+
+class Battleships < Sinatra::Base
+  set :views, Proc.new { File.join(root, "..", "views") }
+
+  get '/' do
+    erb :index
+  end
+
+  get '/game' do
+    
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
